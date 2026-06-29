2f8178121184e91b138cc2a02470e27a96fb987d
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-06-29 23:11:38 +0700

    fix(auth): Return full user payload from JWT strategy

--- server/src/auth/strategy/jwt.strategy.ts
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import type { JwtPayload } from '../../interfaceFile/interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -20,7 +21,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   // This is method that will be called by Passport after it verifies the JWT token. The payload parameter contains the decoded JWT payload.
-  async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+  validate(payload: JwtPayload) {
+    return { userId: payload.userId, email: payload.email };
   }
 }
