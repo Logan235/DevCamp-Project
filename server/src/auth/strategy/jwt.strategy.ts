f018b5f864c0f77ffe849f07459470cfb5572267
BlueDekiVN <an.trinhrankdong@hcmut.edu.vn>
2026-06-30 14:26:10 +0700

    fix: change module to cover all cases

--- server/src/auth/strategy/jwt.strategy.ts
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -5,22 +5,21 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
-    const jwtSecret = configService.get<string>('JWT_SECRET');
-    if (!jwtSecret) {
-      throw new Error(
-        'JWT_SECRET is not defined in the environment variables!',
-      );
-    }
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtSecret,
+      secretOrKey:
+        configService.get<string>('JWT_SECRET') || 'default_backup_secret',
     });
+
+    if (!configService.get<string>('JWT_SECRET')) {
+      console.warn('JWT_SECRET is not defined in env variables!');
+    }
   }
 
   // This is method that will be called by Passport after it verifies the JWT token. The payload parameter contains the decoded JWT payload.
   async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+    return { userId: payload.sub, email: payload.email, role: payload.role };
   }
 }
