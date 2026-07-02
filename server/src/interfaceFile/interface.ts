ee9ff3089be3f355a74208ea057df6c3621f531b
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-07-02 22:37:36 +0700

    add interfae TestCases and field sub

--- server/src/interfaceFile/interface.ts
+++ server/src/interfaceFile/interface.ts
@@ -1,9 +1,20 @@
+import { Types } from 'mongoose';
+
 export interface JwtPayload {
   userId: string;
+  sub: string;
   email?: string;
   role?: string;
 }
 
 export interface RequestWithUser extends Request {
   user: JwtPayload;
+}
+
+export interface TestCases extends Document {
+  _id: Types.ObjectId;
+  challengeId: Types.ObjectId;
+  inputKey?: string;
+  input?: string;
+  expectedOutput: string;
 }
