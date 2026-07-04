b7046f31103393b65a3004202570a1a1bc09f90c
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-07-05 00:58:00 +0700

    import necessary file

--- server/src/exercise/exercise.module.ts
+++ server/src/exercise/exercise.module.ts
@@ -3,17 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ExerciseController } from './exercise.controller';
 import { ExerciseService } from './exercise.service';
 import {
-  Challenge,
-  ChallengeSchema,
   Submission,
   SubmissionSchema,
-} from './exercise.schemas';
+} from '../code-execution/schema/submission.schema'; // <--- Sửa đường dẫn import
+import { Challenge, ChallengeSchema } from './exercise.schemas'; // <--- Giữ lại Challenge
 import {
   UserRoadmap,
   UserRoadmapSchema,
   RoadmapTemplate,
   RoadmapTemplateSchema,
 } from '../roadmap/roadmap.schemas';
+import { TestCase, TestCaseSchema } from '../test/test.schemas';
+import { CodeExecutionModule } from '../code-execution/code-execution.module';
+import { R2Service } from '../shared/r2.service';
 
 @Module({
   imports: [
@@ -23,10 +25,12 @@ import {
 
       { name: UserRoadmap.name, schema: UserRoadmapSchema },
       { name: RoadmapTemplate.name, schema: RoadmapTemplateSchema },
+      { name: TestCase.name, schema: TestCaseSchema },
     ]),
+    CodeExecutionModule,
   ],
   controllers: [ExerciseController],
-  providers: [ExerciseService],
+  providers: [ExerciseService, R2Service],
   exports: [ExerciseService],
 })
 export class ExerciseModule {}
